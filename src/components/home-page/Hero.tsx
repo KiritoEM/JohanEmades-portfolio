afc133a0b0398f2362dc44fdf00d259b3b5a0480
kiritoEM <johankirito64@gmail.com>
2023-11-29 19:20:14 +0100

    add typewriter to hero section-header

--- src/components/home-page/Hero.tsx
+++ src/components/home-page/Hero.tsx
@@ -1,9 +1,51 @@
-const Hero = () :JSX.Element => {
-    return (
-        <section id="home-hero">
-            
-        </section>
-    );
+import Typewriter from "typewriter-effect";
+
+const Hero = (): JSX.Element => {
+  return (
+    <section id="home-hero">
+      <div id="home-hero__container">
+        <div className="row">
+          <div className="col-6">
+            <div className="section-header">
+              <div className="title">
+                <h1>
+                  {" "}
+                  Welcome to MY <span>PORTFOLIO</span>
+                </h1>
+              </div>
+              <div className="subtitle mt-3">
+                <h3>
+                  <Typewriter
+                    options={{
+                      loop: true, // Active la boucle
+                    }}
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString("I'm a Javascript Developer")
+                        .pauseFor(1000)
+                        .deleteAll()
+                        .typeString("I'm fascinated by new technologies")
+                        .pauseFor(1000)
+                        .deleteAll()
+                        .typeString("I search best IT solutions")
+                        .start();
+                    }}
+                  />
+                </h3>
+              </div>
+              <div className="text">
+                <p></p>
+              </div>
+              <div className="button"></div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="section-cover"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import Layout from "./Layout";
 import metaHelper from "@/helpers/metaHelper";
 import PageHeading from "./PageHeading";
+import Hero from "@/components/home-page/Hero";
 
 const homePage = (): JSX.Element => {
   const { metaTitle } = metaHelper();
   return (
     <Layout>
       <PageHeading title={metaTitle.homeTitle} />
-      <section id="home-page"></section>
+      <section id="home-page">
+        {/* ===================home body=============== */}
+        <main>
+          <Hero />
+        </main>
+
+        <footer></footer>
+      </section>
     </Layout>
   );
 };
