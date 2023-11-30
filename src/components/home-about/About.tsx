737e18b8cf468547789d63b64c9623b98664f0d3
kiritoEM <johankirito64@gmail.com>
2023-11-30 06:33:55 +0100

    add header to section-about

--- src/components/home-about/About.tsx
+++ src/components/home-about/About.tsx
@@ -1,27 +1,44 @@
-const About = () :JSX.Element => {
-    return (
-        <section id="home-about">
-            <div id="home-about__container">
-                  <div className="row">
-                     <div className="col-5">
-                         <div className="section-cover">
-                              <img src="/images/p1.jpg" alt="" />
-                         </div>
-                     </div>
-                     <div className="col-6">
-                         <div className="section-header">
-                             <div className="subtitle">
-                                 <h4></h4>
-                             </div>
-                             <div className="title"></div>
-                             <div className="text"></div>
-                             <div className="button"></div>
-                         </div>
-                     </div>
-                  </div>
+const About = (): JSX.Element => {
+  return (
+    <section id="home-about">
+      <div id="home-about__container">
+        <div className="row gx-5">
+          <div className="col-5">
+            <div className="section-cover">
+              <img src="/images/p1.jpg" alt="" />
             </div>
-        </section>
-    );
+          </div>
+          <div className="col-6">
+            <div className="section-header">
+              <div className="subtitle">
+                <div className="spinner"></div>
+                <h4>About me</h4>
+              </div>
+              <div className="title mt-2">
+                <h2>
+                  I am looking for the responsiveness of my{" "}
+                  <span> web apps</span>
+                </h2>
+              </div>
+              <div className="text mt-5">
+                <p>
+                  {" "}
+                  I'm <b> Emadisson Nirina Johannès Loick </b> and I'm 18 years
+                  old. I'm a <b>Javascript Fullstack developer</b> and mostly I
+                  use modern technologies to make my applications efficient,
+                  fast and dynamic . I have a minimum of experience in web
+                  development because now I'm in License 1 and I will pass in
+                  License 2 in next year. Now , I study in ISPM Antsobolo where
+                  I discover and learn new thing about IT development.
+                </p>
+              </div>
+              <div className="button"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
